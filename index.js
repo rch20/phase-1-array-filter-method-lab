1e0f2adab392302ac600d7ea8baf184d1f210cde
rch20 <reemshalak22@hotmail.com>
2022-08-16 00:20:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+
+const findMatching = (drivers, name) =>
+  drivers.filter((driver) => driver.toLowerCase() == name.toLowerCase());
+
+const fuzzyMatch = (drivers, name) =>
+  drivers.filter(
+    (driver) => driver.toLowerCase().indexOf(name.toLowerCase()) === 0
+  );
+
+const matchName = (drivers, name) =>
+  drivers.filter((driver) => driver.name.toLowerCase() == name.toLowerCase());
